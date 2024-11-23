3a8967d8c23c678667e3f644e39ecb5a7b6e334e
Patricia Lopez <89365508+plopez1985@users.noreply.github.com>
2024-11-23 17:40:08 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "ejemplo"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".net", "org"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//interamos en el array1
+//1era interacion del arra1 vamos arecorrer los dos ARRAYS INTERINOS
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < domain.length; l++) {
+        //eslint-disable-next-line no-console
+        console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
+      }
+    }
+  }
+}
